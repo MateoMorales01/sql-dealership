a22258a1b07a2832de3953c040532a4cf2f523b8
MateoMorales01 <moralmat76@gmail.com>
2025-11-25 17:34:02 -0600

    Add files via upload

---
+++ dealership and vehicles generated.sql
@@ -0,0 +1,399 @@
+use car_dealership;
+
+insert into dealerships (dealership_id, name, address, phone)
+values(1004, "AutoNation", "12 turtle rd", "(555)-487-0942");
+
+insert into vehicles(make, model, vehicletype, color, odometer, price, vin, year)
+values (
+'Honda', 'CR-V', 'SUV', 'Silver', 25400, 27500.00, 940021234, 2022
+),
+(
+'Ford', 'F-150', 'Truck', 'Black', 48900, 42100.00, 940021235, 2020
+),
+(
+'Toyota', 'Camry', 'Sedan', 'White', 12300, 29800.00, 940021236, 2023
+),
+(
+'BMW', 'X5', 'SUV', 'Blue', 67000, 31900.00, 940021237, 2018
+),
+(
+'Nissan', 'Rogue', 'SUV', 'Gray', 32500, 24500.00, 940021238, 2021
+),
+(
+'Jeep', 'Wrangler', 'SUV', 'Red', 7500, 41000.00, 940021239, 2024
+),
+(
+'Tesla', 'Model 3', 'Sedan', 'Black', 15000, 36800.00, 940021240, 2023
+),
+(
+'Subaru', 'Outback', 'Wagon', 'Green', 55200, 22400.00, 940021241, 2019
+),
+(
+'Hyundai', 'Elantra', 'Sedan', 'Silver', 21100, 19500.00, 940021242, 2022
+),
+(
+'Dodge', 'Challenger', 'Coupe', 'Orange', 39800, 33200.00, 940021243, 2020
+),
+(
+'Mercedes-Benz', 'C-Class', 'Sedan', 'White', 59000, 28700.00, 940021244, 2018
+),
+(
+'Honda', 'Civic', 'Sedan', 'Black', 18600, 23900.00, 940021245, 2023
+),
+(
+'Ford', 'Explorer', 'SUV', 'Blue', 88000, 15500.00, 940021246, 2017
+),
+(
+'Toyota', 'Tacoma', 'Truck', 'Gray', 41200, 34000.00, 940021247, 2021
+),
+(
+'Audi', 'A4', 'Sedan', 'Red', 35000, 30500.00, 940021248, 2020
+),
+(
+'Kia', 'Telluride', 'SUV', 'White', 10500, 38900.00, 940021249, 2023
+),
+(
+'Chevrolet', 'Silverado', 'Truck', 'Black', 62100, 37500.00, 940021250, 2019
+),
+(
+'Mazda', 'CX-5', 'SUV', 'Red', 28900, 26900.00, 940021251, 2022
+),
+(
+'Volvo', 'S60', 'Sedan', 'Silver', 45000, 25000.00, 940021252, 2019
+),
+(
+'Volkswagen', 'Jetta', 'Sedan', 'Blue', 78000, 14900.00, 940021253, 2016
+),
+(
+'Honda', 'Pilot', 'SUV', 'Gray', 11000, 37200.00, 940021254, 2024
+),
+(
+'Ford', 'Mustang', 'Coupe', 'Yellow', 29500, 34900.00, 940021255, 2021
+),
+(
+'Toyota', 'Highlander', 'SUV', 'Black', 49900, 31500.00, 940021256, 2020
+),
+(
+'BMW', '3 Series', 'Sedan', 'White', 60000, 26000.00, 940021257, 2018
+),
+(
+'Nissan', 'Altima', 'Sedan', 'Silver', 38000, 19900.00, 940021258, 2021
+),
+(
+'Jeep', 'Cherokee', 'SUV', 'Red', 85000, 12500.00, 940021259, 2016
+),
+(
+'Tesla', 'Model Y', 'SUV', 'Gray', 5000, 48000.00, 940021260, 2024
+),
+(
+'Subaru', 'Forester', 'SUV', 'Green', 22000, 28900.00, 940021261, 2022
+),
+(
+'Hyundai', 'Tucson', 'SUV', 'Blue', 52000, 21000.00, 940021262, 2019
+),
+(
+'Dodge', 'Ram 1500', 'Truck', 'White', 71000, 32000.00, 940021263, 2018
+),
+(
+'Mercedes-Benz', 'GLC', 'SUV', 'Black', 30000, 39500.00, 940021264, 2021
+),
+(
+'Honda', 'Accord', 'Sedan', 'Gray', 14500, 28500.00, 940021265, 2023
+),
+(
+'Ford', 'Escape', 'SUV', 'Silver', 65000, 16900.00, 940021266, 2017
+),
+(
+'Toyota', '4Runner', 'SUV', 'Black', 51000, 35500.00, 940021267, 2020
+),
+(
+'Audi', 'Q5', 'SUV', 'White', 27000, 38500.00, 940021268, 2022
+),
+(
+'Kia', 'Sorento', 'SUV', 'Red', 44000, 25900.00, 940021269, 2020
+),
+(
+'Chevrolet', 'Malibu', 'Sedan', 'Blue', 95000, 10500.00, 940021270, 2015
+),
+(
+'Mazda', 'Mazda3', 'Sedan', 'Silver', 20000, 21900.00, 940021271, 2023
+),
+(
+'Volvo', 'XC90', 'SUV', 'Gray', 55000, 33500.00, 940021272, 2019
+),
+(
+'Volkswagen', 'Tiguan', 'SUV', 'White', 33000, 24000.00, 940021273, 2021
+),
+(
+'Honda', 'Odyssey', 'Minivan', 'Blue', 58000, 26500.00, 940021274, 2019
+),
+(
+'Ford', 'Edge', 'SUV', 'Black', 41000, 23500.00, 940021275, 2020
+),
+(
+'Toyota', 'Sienna', 'Minivan', 'Silver', 18000, 36900.00, 940021276, 2022
+),
+(
+'BMW', '5 Series', 'Sedan', 'Black', 72000, 23000.00, 940021277, 2017
+),
+(
+'Nissan', 'Titan', 'Truck', 'Red', 59000, 31000.00, 940021278, 2019
+),
+(
+'Jeep', 'Renegade', 'SUV', 'Yellow', 35000, 18900.00, 940021279, 2021
+),
+(
+'Tesla', 'Model S', 'Sedan', 'White', 40000, 45000.00, 940021280, 2018
+),
+(
+'Subaru', 'Impreza', 'Sedan', 'Silver', 65000, 15500.00, 940021281, 2017
+),
+(
+'Hyundai', 'Kona', 'SUV', 'Gray', 11500, 23500.00, 940021282, 2023
+),
+(
+'Dodge', 'Durango', 'SUV', 'Black', 53000, 28000.00, 940021283, 2020
+),
+(
+'Mercedes-Benz', 'E-Class', 'Sedan', 'Blue', 48000, 32900.00, 940021284, 2020
+),
+(
+'Honda', 'HR-V', 'SUV', 'Red', 29000, 21500.00, 940021285, 2022
+),
+(
+'Ford', 'Bronco', 'SUV', 'Green', 5000, 45000.00, 940021286, 2024
+),
+(
+'Toyota', 'Corolla', 'Sedan', 'White', 31000, 20500.00, 940021287, 2021
+),
+(
+'Audi', 'A6', 'Sedan', 'Black', 77000, 22500.00, 940021288, 2017
+),
+(
+'Kia', 'Sportage', 'SUV', 'Gray', 16000, 28900.00, 940021289, 2023
+),
+(
+'Chevrolet', 'Equinox', 'SUV', 'Silver', 68000, 14900.00, 940021290, 2018
+),
+(
+'Mazda', 'CX-9', 'SUV', 'Blue', 45000, 27500.00, 940021291, 2020
+),
+(
+'Volvo', 'V60', 'Wagon', 'White', 38000, 28000.00, 940021292, 2021
+),
+(
+'Volkswagen', 'Passat', 'Sedan', 'Red', 82000, 13500.00, 940021293, 2016
+),
+(
+'Honda', 'Ridgeline', 'Truck', 'Black', 22000, 35000.00, 940021294, 2022
+),
+(
+'Ford', 'Ranger', 'Truck', 'Gray', 50000, 28500.00, 940021295, 2019
+),
+(
+'Toyota', 'Prius', 'Hatchback', 'Silver', 75000, 16900.00, 940021296, 2017
+),
+(
+'BMW', 'X3', 'SUV', 'Blue', 43000, 34000.00, 940021297, 2020
+),
+(
+'Nissan', 'Murano', 'SUV', 'White', 60000, 17500.00, 940021298, 2018
+),
+(
+'Jeep', 'Grand Cherokee', 'SUV', 'Black', 29000, 39900.00, 940021299, 2022
+),
+(
+'Tesla', 'Model X', 'SUV', 'Red', 55000, 52000.00, 940021300, 2017
+),
+(
+'Subaru', 'Crosstrek', 'SUV', 'Yellow', 16000, 26500.00, 940021301, 2023
+),
+(
+'Hyundai', 'Palisade', 'SUV', 'White', 30000, 34500.00, 940021302, 2021
+),
+(
+'Dodge', 'Charger', 'Sedan', 'Gray', 64000, 25000.00, 940021303, 2019
+),
+(
+'Mercedes-Benz', 'GLA', 'SUV', 'Silver', 24000, 31900.00, 940021304, 2022
+),
+(
+'Honda', 'Fit', 'Hatchback', 'Green', 98000, 11000.00, 940021305, 2015
+),
+(
+'Ford', 'Maverick', 'Truck', 'Blue', 8000, 28500.00, 940021306, 2023
+),
+(
+'Toyota', 'Camry', 'Sedan', 'Black', 40000, 22500.00, 940021307, 2020
+),
+(
+'Audi', 'Q3', 'SUV', 'Red', 19000, 32900.00, 940021308, 2023
+),
+(
+'Kia', 'Forte', 'Sedan', 'White', 48000, 17500.00, 940021309, 2021
+),
+(
+'Chevrolet', 'Tahoe', 'SUV', 'Black', 70000, 38000.00, 940021310, 2018
+),
+(
+'Mazda', 'Mazda6', 'Sedan', 'Gray', 54000, 18900.00, 940021311, 2019
+),
+(
+'Volvo', 'S90', 'Sedan', 'Silver', 62000, 24500.00, 940021312, 2017
+),
+(
+'Volkswagen', 'Atlas', 'SUV', 'White', 27000, 33500.00, 940021313, 2022
+),
+(
+'Honda', 'Insight', 'Sedan', 'Blue', 42000, 20500.00, 940021314, 2020
+),
+(
+'Ford', 'Fusion', 'Sedan', 'Gray', 90000, 10900.00, 940021315, 2016
+),
+(
+'Toyota', 'Tundra', 'Truck', 'Red', 35000, 45000.00, 940021316, 2021
+),
+(
+'BMW', '4 Series', 'Coupe', 'Black', 51000, 30500.00, 940021317, 2019
+),
+(
+'Nissan', 'Kicks', 'SUV', 'Silver', 15000, 21900.00, 940021318, 2023
+),
+(
+'Jeep', 'Compass', 'SUV', 'White', 38000, 22000.00, 940021319, 2021
+),
+(
+'Tesla', 'Roadster', 'Convertible', 'Red', 10000, 65000.00, 940021320, 2015
+),
+(
+'Subaru', 'Legacy', 'Sedan', 'Green', 70000, 15000.00, 940021321, 2017
+),
+(
+'Hyundai', 'Santa Fe', 'SUV', 'Black', 26000, 29500.00, 940021322, 2022
+),
+(
+'Dodge', 'Journey', 'SUV', 'Blue', 110000, 8900.00, 940021323, 2015
+),
+(
+'Mercedes-Benz', 'A-Class', 'Sedan', 'White', 12000, 34000.00, 940021324, 2024
+),
+(
+'Honda', 'CR-V', 'SUV', 'Gray', 68000, 17900.00, 940021325, 2017
+),
+(
+'Ford', 'F-250', 'Truck', 'White', 85000, 42000.00, 940021326, 2016
+),
+(
+'Toyota', 'Venza', 'SUV', 'Black', 20000, 34500.00, 940021327, 2022
+),
+(
+'Audi', 'A3', 'Sedan', 'Silver', 49000, 25500.00, 940021328, 2019
+),
+(
+'Kia', 'Rio', 'Sedan', 'Red', 70000, 12000.00, 940021329, 2018
+),
+(
+'Chevrolet', 'Camaro', 'Coupe', 'Yellow', 35000, 36000.00, 940021330, 2020
+),
+(
+'Mazda', 'CX-30', 'SUV', 'Blue', 18000, 25900.00, 940021331, 2023
+),
+(
+'Volvo', 'XC60', 'SUV', 'Black', 40000, 30000.00, 940021332, 2021
+),
+(
+'Volkswagen', 'Golf', 'Hatchback', 'Gray', 55000, 16500.00, 940021333, 2018
+),
+(
+'Honda', 'Pilot', 'SUV', 'White', 78000, 19500.00, 940021334, 2016
+),
+(
+'Ford', 'Edge', 'SUV', 'Red', 12000, 33500.00, 940021335, 2023
+),
+(
+'Toyota', 'Sienna', 'Minivan', 'Silver', 65000, 23000.00, 940021336, 2019
+),
+(
+'BMW', 'X1', 'SUV', 'Black', 22000, 30900.00, 940021337, 2022
+),
+(
+'Nissan', 'Frontier', 'Truck', 'Gray', 40000, 29500.00, 940021338, 2021
+),
+(
+'Jeep', 'Wagoneer', 'SUV', 'White', 5000, 65000.00, 940021339, 2024
+),
+(
+'Tesla', 'Model 3', 'Sedan', 'Blue', 30000, 32000.00, 940021340, 2021
+),
+(
+'Subaru', 'WRX', 'Sedan', 'White', 55000, 25000.00, 940021341, 2019
+),
+(
+'Hyundai', 'Venue', 'SUV', 'Red', 10000, 21500.00, 940021342, 2023
+),
+(
+'Dodge', 'Ram 3500', 'Truck', 'Black', 95000, 35000.00, 940021343, 2017
+),
+(
+'Mercedes-Benz', 'S-Class', 'Sedan', 'Silver', 80000, 39900.00, 940021344, 2016
+),
+(
+'Honda', 'Civic', 'Sedan', 'Gray', 50000, 18500.00, 940021345, 2019
+),
+(
+'Ford', 'Bronco Sport', 'SUV', 'Green', 14000, 31900.00, 940021346, 2023
+),
+(
+'Toyota', 'Rav4', 'SUV', 'Black', 30000, 28500.00, 940021347, 2021
+),
+(
+'Audi', 'Q7', 'SUV', 'White', 60000, 33000.00, 940021348, 2018
+),
+(
+'Kia', 'K5', 'Sedan', 'Blue', 25000, 24900.00, 940021349, 2022
+),
+(
+'Chevrolet', 'Cruze', 'Sedan', 'Silver', 115000, 7500.00, 940021350, 2015
+),
+(
+'Mazda', 'MX-5 Miata', 'Convertible', 'Red', 45000, 27000.00, 940021351, 2020
+),
+(
+'Volvo', 'S60', 'Sedan', 'Gray', 18000, 35000.00, 940021352, 2023
+),
+(
+'Volkswagen', 'Arteon', 'Sedan', 'Black', 28000, 29900.00, 940021353, 2021
+),
+(
+'Honda', 'Accord', 'Sedan', 'White', 95000, 13000.00, 940021354, 2016
+),
+(
+'Ford', 'Escape', 'SUV', 'Blue', 35000, 24000.00, 940021355, 2022
+),
+(
+'Toyota', 'Highlander', 'SUV', 'Silver', 72000, 23500.00, 940021356, 2018
+),
+(
+'BMW', '7 Series', 'Sedan', 'Black', 88000, 31000.00, 940021357, 2017
+),
+(
+'Nissan', 'Armada', 'SUV', 'Red', 50000, 36000.00, 940021358, 2020
+),
+(
+'Jeep', 'Gladiator', 'Truck', 'Gray', 15000, 42000.00, 940021359, 2023
+),
+(
+'Tesla', 'Model S', 'Sedan', 'White', 22000, 58000.00, 940021360, 2022
+),
+(
+'Subaru', 'Ascent', 'SUV', 'Blue', 45000, 28000.00, 940021361, 2019
+),
+(
+'Hyundai', 'Elantra', 'Sedan', 'Black', 60000, 15500.00, 940021362, 2018
+),
+(
+'Dodge', 'Viper', 'Coupe', 'Yellow', 15000, 85000.00, 940021363, 2015
+),
+(
+'Mercedes-Benz', 'GLE', 'SUV', 'White', 33000, 45000.00, 940021364, 2021
+)
